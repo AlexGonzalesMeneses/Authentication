8f844e3c92f77022a1096ceea62338f1234eec7c
Gustavo Omonte Acevedo <gustavo.omonte@fundacion-jala.org>
2022-02-07 16:34:03 -0400

    Base Esctructure Frontend

---
+++ client/src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Login from './components/Login';
+const App = () => (
+  <div>
+    <Login></Login>
+    <h1>Hello React</h1>
+  </div>
+);
+
+export default App;
