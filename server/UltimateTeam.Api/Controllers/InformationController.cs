d42b5dea9e04f0b740d57a2aa5a7d17ec49ff436
Gustavo Omonte Acevedo <gustavo.omonte@fundacion-jala.org>
2022-03-02 23:51:57 -0400

    feature Search Backend finish

--- server/UltimateTeam.Api/Controllers/InformationController.cs
+++ server/UltimateTeam.Api/Controllers/InformationController.cs
@@ -1,12 +1,15 @@
 using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using AutoMapper;
 using Dev33.UltimateTeam.Api.Services.LoggerService;
 using Dev33.UltimateTeam.Application.Contracts.Services;
+using Dev33.UltimateTeam.Application.Helpers;
 using Dev33.UltimateTeam.Domain.Models;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using UltimateTeam.Application.Dtos;
 
 namespace UltimateTeam.Api.Controllers
 {
@@ -17,19 +20,24 @@ namespace UltimateTeam.Api.Controllers
     {
         private readonly ISearchService searchService;
         private readonly ILoggerManager logger;
+        private readonly IMapper mapper;
 
-        public InformationController(ISearchService searchService, ILoggerManager logger)
+        public InformationController(ISearchService searchService, ILoggerManager logger, IMapper mapper)
         {
             this.searchService = searchService;
             this.logger = logger;
+            this.mapper = mapper;
         }
 
         [HttpGet("searchTerm")]
-        public async Task<IEnumerable<Information>> Search(Guid userId, [FromQuery] string searchTerm)
+        public async Task<IEnumerable<InformationResponseDto>> Search(Guid userId, [FromQuery] string searchTerm)
         {
             try
             {
-                return await searchService.Search(userId, searchTerm);
+                var informations = await searchService.Search(userId, searchTerm);
+                var response = InformationMapper.Map(informations);
+
+                return response;
             }
             catch (Exception ex)
             {
